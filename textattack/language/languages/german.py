d1881e5365a21dd5a2ee8373e34ea2fe91de2044
Matthias Bisping <matthias.bisping@axxessio.com>
2020-12-28 12:07:41 +0100

    word2vec download logic works now

--- textattack/language/languages/german.py
+++ textattack/language/languages/german.py
@@ -1,4 +1,4 @@
-from textattack.language.languageresourceprovision import LanguageResourceProvision
+from textattack.language.language_resource_provision import LanguageResourceProvision
 from textattack.transformations.word_swaps.word_swap_embedding import WordSwapEmbedding
 from textattack.shared import GensimWordEmbedding
 from textattack.language.languages.resource_provision import provide_word2vec
--- textattack/language/languages/resource_provision.py
+++ textattack/language/languages/resource_provision.py
@@ -1,14 +1,14 @@
 import gzip
 import os
 import shutil
+import sys
 import urllib.request
+from pathlib import Path
 
-from gensim.models import KeyedVectors
+from gensim.models.keyedvectors import Word2VecKeyedVectors
 from tqdm import tqdm
 
 from textattack.shared.utils.install import path_in_cache
-
-from gensim.models.keyedvectors import Word2VecKeyedVectors
 
 
 def read_config():
@@ -18,20 +18,23 @@ def read_config():
     return config
 
 
-def provide_word2vec(embedding_name='cc.en.300.vec.gz'):
+def provide_word2vec(embedding_name='cc.en.300.vec'):
     return Word2VecKeyedVectors.load(provide_word2vec_file(embedding_name))
 
 
-def provide_word2vec_file(embedding_name):
+def provide_word2vec_file(embedding_name: str) -> str:
+    """Provides the path to a local embedding file for the word2vec fasttext embedding `embedding_name`. If file is not
+    present, the embedding is downloaded from the server first.
+
+    Args:
+        embedding_name: name of embedding (e.g. 'cc.en.300.vec') to download from server (see https://fasttext.cc).
+
+    Returns: path file embedding file
+    """
+
     # TODO: generalize to support fasttext OR word2vec
     def download_word2vec():
-        """Download, extract and pickle word2vec embedding. clean up afterwards.
-
-        Args:
-            embedding_name: name of embedding (e.g. 'cc.en.300.vec') to download from fastext website (https://fasttext.cc).
-
-        Returns: None
-        """
+        """Download, extract and pickle word2vec embedding. clean up afterwards."""
 
         class DownloadProgressBar(tqdm):
             def update_to(self, b=1, bsize=1, tsize=None):
@@ -44,24 +47,23 @@ def provide_word2vec_file(embedding_name):
                 urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
 
         def download():
-            print(f'Downloading embedding and saving as {embedding_path_gz} ...', file=sys.stderr)
+            print(f'Downloading embedding and saving as {embedding_path_gz}...', file=sys.stderr)
             download_to(embedding_url, embedding_path_gz)
 
         def extract():
-            print(f'Extracting embedding to {embedding_path_vec} ...', file=sys.stderr)
+            print(f'Extracting embedding to {embedding_path_vec}...', file=sys.stderr)
             with gzip.open(embedding_path_gz, 'rb') as f_in:
                 with open(embedding_path_vec, 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
 
         def pickle():
-            print(f'Saving final embedding as {embedding_path_m} ...', file=sys.stderr)
-            embedding = KeyedVectors.load_word2vec_format(embedding_path_vec)
+            print(f'Saving final embedding as {embedding_path_m}... (This may take a while.)', file=sys.stderr)
+            embedding = Word2VecKeyedVectors.load_word2vec_format(embedding_path_vec)
             embedding.save(embedding_path_m)
 
         def clean_up():
-            tmp_folder = os.path.join(download_path, 'tmp')
-            for filename in os.listdir(tmp_folder):
-                file_path = os.path.join(tmp_folder, filename)
+            for filename in os.listdir(tmp_path):
+                file_path = os.path.join(tmp_path, filename)
                 try:
                     if os.path.isfile(file_path) or os.path.islink(file_path):
                         os.unlink(file_path)
@@ -69,16 +71,19 @@ def provide_word2vec_file(embedding_name):
                         shutil.rmtree(file_path)
                 except Exception as e:
                     print('Failed to delete %s. Reason: %s' % (file_path, e))
+            shutil.rmtree(tmp_path)
 
-        if not os.path.exists(embedding_path_m):
-            download()
-            extract()
-            pickle()
-            clean_up()
+        Path(os.path.join(tmp_path)).mkdir(parents=True, exist_ok=True)
+        download()
+        extract()
+        pickle()
+        clean_up()
 
     embedding_url = f'https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/{embedding_name}.gz'
     download_path = path_in_cache('word2vec')
-    embedding_path_gz = os.path.join(download_path, 'tmp', embedding_name)
+
+    tmp_path = os.path.join(download_path, 'tmp')
+    embedding_path_gz = os.path.join(tmp_path, embedding_name+'.gz')
     embedding_path_vec = os.path.splitext(embedding_path_gz)[0]
     embedding_path_m = os.path.join(download_path, os.path.basename(os.path.splitext(embedding_path_vec)[0] + '.m'))
 
